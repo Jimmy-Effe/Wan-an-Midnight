8abe11875bec6d83566561ada6ad87d9882774e3
Junchen Yi <63986407+Jimmy-Effe@users.noreply.github.com>
2022-08-27 15:05:37 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,5 @@
+ // * @Author       : Effe-jimmy
+ // * @Designer	    : Taohua-Nong
+ // * @Date         : 2022-07-15 21:11:30
+ // * @Program name : index.js
+ // * Copyright (c) 2022 by Effe-jimmy Taohua-Nong, All Rights Reserved. 

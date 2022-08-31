37152322455e22cf3d472a1c58acf31fa998b481
Junchen Yi <63986407+Jimmy-Effe@users.noreply.github.com>
2022-08-31 19:25:54 +0800

    Add files via upload

---
+++ js/ranking.js
@@ -0,0 +1,215 @@
+const URL =
+"https://pixel.nymag.com/imgs/daily/vulture/2018/07/08/8-natalie.w330.h330.jpg";
+const TM1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+const TM2 =
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed interdum libero. Suspendisse sed interdum libero. Ut fermentum, nulla id fermentum condimentum, libero metus dictum risus, et lobortis mi purus nec metus.";
+var siteStatsData = {
+  "Guatemala":
+  {
+    "Chat":
+    {
+      "topPerformers":
+      [
+      [0, URL, "Natalie Dormer", 1, 1, 1, 150, [TM1, TM2]],
+      [1, "https://i.imgur.com/uD5Wq.jpg", "Gary Oldman", 2, 0, 1, 125, [TM1, TM2]],
+      [2, "https://img.zergnet.com/603861_300.jpg", "Ron Perlman", 3, -1, 0.995, 120, [TM1, TM2]],
+      [3, "https://www.earnthenecklace.com/wp-content/uploads/2016/05/Dakota-Johnson-300x300.jpg", "Dakota Johnson", 4, -1, 0.9921, 100, [TM1, TM2]],
+      [4, "https://mediamass.net/jdd/public/documents/celebrities/297.jpg", "Robin Williams", 5, 1, 0.99, 94, [TM1, TM2]],
+      [5, "https://images.eonline.com/eol_images/Entire_Site/2014726/rs_300x300-140826115133-result_2.jpg?fit=inside|300:300&output-quality=90", "Ximena Sariñana", 6, 1, 0.99, 28, [TM1, TM2]],
+      [6, "https://www.mp3eagle.com/covers/50/50/art_50_big2.jpg", "Eithne Pádraigín Ní Bhraonáin", 7, 1, 0.99, 120, [TM1, TM2]],
+      [7, "https://static3.therichestimages.com/wordpress/wp-content/uploads/Juan-Luis-Guerra.jpg", "Juan Luis Guerra", 8, 1, 0.99, 151, [TM1, TM2]]],
+
+      "bottomPerformers":
+      [
+      [100, URL, "Gua - Chat Agent Full Name 100", 100, 1, 0, 40, [TM1, TM2]],
+      [99, URL, "Gua - Chat Agent Full Name 99", 99, 0, 0, 38, [TM1, TM2]],
+      [98, URL, "Gua - Chat Agent Full Name 98", 98, -1, 0.125, 125, [TM1, TM2]],
+      [97, URL, "Gua - Chat Agent Full Name 97", 97, -1, 0.128, 120, [TM1, TM2]],
+      [96, URL, "Gua - Chat Agent Full Name 96", 96, 1, 0.13, 78, [TM1, TM2]],
+      [95, URL, "Gua - Chat Agent Full Name 95", 95, 1, 0.14, 38, [TM1, TM2]],
+      [94, URL, "Gua - Chat Agent Full Name 94", 94, 1, 0.15, 115, [TM1, TM2]],
+      [93, URL, "Gua - Chat Agent Full Name 93", 93, 1, 0.16, 130, [TM1, TM2]]] },
+
+
+    "Email": {
+      "topPerformers":
+      [
+      [0, URL, "Natalie Dormer", 1, 1, 1, 150, [TM1, TM2]],
+      [1, "https://i.imgur.com/uD5Wq.jpg", "Gary Oldman", 2, 0, 1, 125, [TM1, TM2]],
+      [2, "https://img.zergnet.com/603861_300.jpg", "Ron Perlman", 3, -1, 0.995, 120, [TM1, TM2]],
+      [3, "https://www.earnthenecklace.com/wp-content/uploads/2016/05/Dakota-Johnson-300x300.jpg", "Dakota Johnson", 4, -1, 0.9921, 100, [TM1, TM2]],
+      [4, "https://mediamass.net/jdd/public/documents/celebrities/297.jpg", "Robin Williams", 5, 1, 0.99, 94, [TM1, TM2]],
+      [5, "https://images.eonline.com/eol_images/Entire_Site/2014726/rs_300x300-140826115133-result_2.jpg?fit=inside|300:300&output-quality=90", "Ximena Sariñana", 6, 1, 0.99, 28, [TM1, TM2]],
+      [6, "https://www.mp3eagle.com/covers/50/50/art_50_big2.jpg", "Eithne Pádraigín Ní Bhraonáin", 7, 1, 0.99, 120, [TM1, TM2]],
+      [7, "https://static3.therichestimages.com/wordpress/wp-content/uploads/Juan-Luis-Guerra.jpg", "Juan Luis Guerra", 8, 1, 0.99, 151, [TM1, TM2]]],
+
+      "bottomPerformers":
+      [
+      [100, URL, "Gua - Email Agent Full Name 100", 100, 1, 0, 40, [TM1, TM2]],
+      [99, URL, "Gua - Email Agent Full Name 99", 99, 0, 0, 38, [TM1, TM2]],
+      [98, URL, "Gua - Email Agent Full Name 98", 98, -1, 0.125, 125, [TM1, TM2]],
+      [97, URL, "Gua - Email Agent Full Name 97", 97, -1, 0.128, 120, [TM1, TM2]],
+      [96, URL, "Gua - Email Agent Full Name 96", 96, 1, 0.13, 78, [TM1, TM2]],
+      [95, URL, "Gua - Email Agent Full Name 95", 95, 1, 0.14, 38, [TM1, TM2]],
+      [94, URL, "Gua - Email Agent Full Name 94", 94, 1, 0.15, 115, [TM1, TM2]],
+      [93, URL, "Gua - Email Agent Full Name 93", 93, 1, 0.16, 130, [TM1, TM2]]] },
+
+
+    "Phone": {
+      "topPerformers":
+      [
+      [0, URL, "Natalie Dormer", 1, 1, 1, 150, [TM1, TM2]],
+      [1, "https://i.imgur.com/uD5Wq.jpg", "Gary Oldman", 2, 0, 1, 125, [TM1, TM2]],
+      [2, "https://img.zergnet.com/603861_300.jpg", "Ron Perlman", 3, -1, 0.995, 120, [TM1, TM2]],
+      [3, "https://www.earnthenecklace.com/wp-content/uploads/2016/05/Dakota-Johnson-300x300.jpg", "Dakota Johnson", 4, -1, 0.9921, 100, [TM1, TM2]],
+      [4, "https://mediamass.net/jdd/public/documents/celebrities/297.jpg", "Robin Williams", 5, 1, 0.99, 94, [TM1, TM2]],
+      [5, "https://images.eonline.com/eol_images/Entire_Site/2014726/rs_300x300-140826115133-result_2.jpg?fit=inside|300:300&output-quality=90", "Ximena Sariñana", 6, 1, 0.99, 28, [TM1, TM2]],
+      [6, "https://www.mp3eagle.com/covers/50/50/art_50_big2.jpg", "Eithne Pádraigín Ní Bhraonáin", 7, 1, 0.99, 120, [TM1, TM2]],
+      [7, "https://static3.therichestimages.com/wordpress/wp-content/uploads/Juan-Luis-Guerra.jpg", "Juan Luis Guerra", 8, 1, 0.99, 151, [TM1, TM2]]],
+
+      "bottomPerformers":
+      [
+      [100, URL, "Gua - Phone Agent Full Name 100", 100, 1, 0, 40, [TM1, TM2]],
+      [99, URL, "Gua - Phone Agent Full Name 99", 99, 0, 0, 38, [TM1, TM2]],
+      [98, URL, "Gua - Phone Agent Full Name 98", 98, -1, 0.125, 125, [TM1, TM2]],
+      [97, URL, "Gua - Phone Agent Full Name 97", 97, -1, 0.128, 120, [TM1, TM2]],
+      [96, URL, "Gua - Phone Agent Full Name 96", 96, 1, 0.13, 78, [TM1, TM2]],
+      [95, URL, "Gua - Phone Agent Full Name 95", 95, 1, 0.14, 38, [TM1, TM2]],
+      [94, URL, "Gua - Phone Agent Full Name 94", 94, 1, 0.15, 115, [TM1, TM2]],
+      [93, URL, "Gua - Phone Agent Full Name 93", 93, 1, 0.16, 130, [TM1, TM2]]] } },
+
+
+
+  "Tampa":
+  {
+    "Phone":
+    {
+      "topPerformers":
+      [
+      [0, URL, "Natalie Dormer", 1, 1, 1, 150, [TM1, TM2]],
+      [1, "https://i.imgur.com/uD5Wq.jpg", "Gary Oldman", 2, 0, 1, 125, [TM1, TM2]],
+      [2, "https://img.zergnet.com/603861_300.jpg", "Ron Perlman", 3, -1, 0.995, 120, [TM1, TM2]],
+      [3, "https://www.earnthenecklace.com/wp-content/uploads/2016/05/Dakota-Johnson-300x300.jpg", "Dakota Johnson", 4, -1, 0.9921, 100, [TM1, TM2]],
+      [4, "https://mediamass.net/jdd/public/documents/celebrities/297.jpg", "Robin Williams", 5, 1, 0.99, 94, [TM1, TM2]],
+      [5, "https://images.eonline.com/eol_images/Entire_Site/2014726/rs_300x300-140826115133-result_2.jpg?fit=inside|300:300&output-quality=90", "Ximena Sariñana", 6, 1, 0.99, 28, [TM1, TM2]],
+      [6, "https://www.mp3eagle.com/covers/50/50/art_50_big2.jpg", "Eithne Pádraigín Ní Bhraonáin", 7, 1, 0.99, 120, [TM1, TM2]],
+      [7, "https://static3.therichestimages.com/wordpress/wp-content/uploads/Juan-Luis-Guerra.jpg", "Juan Luis Guerra", 8, 1, 0.99, 151, [TM1, TM2]]],
+
+      "bottomPerformers":
+      [
+      [100, URL, "Tpa - Phone Agent Full Name 100", 100, 1, 0, 40, [TM1, TM2]],
+      [99, URL, "Tpa - Phone Agent Full Name 99", 99, 0, 0, 38, [TM1, TM2]],
+      [98, URL, "Tpa - Phone Agent Full Name 98", 98, -1, 0.125, 125, [TM1, TM2]],
+      [97, URL, "Tpa - Phone Agent Full Name 97", 97, -1, 0.128, 120, [TM1, TM2]],
+      [96, URL, "Tpa - Phone Agent Full Name 96", 96, 1, 0.13, 78, [TM1, TM2]],
+      [95, URL, "Tpa - Phone Agent Full Name 95", 95, 1, 0.14, 38, [TM1, TM2]],
+      [94, URL, "Tpa - Phone Agent Full Name 94", 94, 1, 0.15, 115, [TM1, TM2]],
+      [93, URL, "Tpa - Phone Agent Full Name 93", 93, 1, 0.16, 130, [TM1, TM2]]] } } };
+
+
+
+
+
+// Initial Data
+let site = Object.keys(siteStatsData)[0];
+let lob = Object.keys(siteStatsData[site])[0];
+let rowTop = siteStatsData[site][lob].topPerformers;
+let rowBottom = siteStatsData[site][lob].bottomPerformers;
+let row = rowTop[0];
+
+let app = new Vue({
+  el: "#app",
+  data: {
+    scoreNum: 0,
+    tweenedScore: row[5],
+    surveysNum: 0,
+    tweenedSurveys: row[6],
+    topPerformers: rowTop,
+    bottomPerformers: rowBottom,
+    currentData: {
+      site,
+      lob,
+      agentName: row[2],
+      agentId: row[0],
+      agentPicUrl: row[1],
+      agentRank: row[3],
+      agentTrend: row[4],
+      agentSurveys: row[6],
+      testimonial: row[7][0],
+      testimonialId: 0 },
+
+    siteStats: siteStatsData },
+
+  filters: {
+    toPercentage: function (n) {
+      let val = n * 100;
+      return `${val.toFixed(2)}%`;
+    } },
+
+  watch: {
+    scoreNum: function (newValue) {
+      TweenLite.to(this.$data, 0.5, { tweenedScore: newValue });
+    },
+    surveysNum: function (newValue) {
+      TweenLite.to(this.$data, 0.5, { tweenedSurveys: newValue });
+    } },
+
+  computed: {
+    tweenedScore: function () {
+      let val = this.tweenedScore * 100;
+      return val.toFixed(2);
+    } } });
+
+
+
+function runMarquee(obj, siteIndx, lobIndx, rowIndx, testIndx) {
+  let siteLen = Object.keys(obj).length;
+  if (siteIndx === siteLen) {
+    runMarquee(obj, 0, 0, 7, 0);
+    return;
+  }
+
+  let siteKey = Object.keys(obj)[siteIndx];
+  let lobLen = Object.keys(obj[siteKey]).length;
+
+  if (lobIndx === lobLen) {
+    runMarquee(obj, ++siteIndx, 0, 7, 0);
+    return;
+  }
+
+  let lobKey = Object.keys(obj[siteKey])[lobIndx];
+  let rows = obj[siteKey][lobKey].topPerformers;
+
+  if (rowIndx < 0) {
+    let newLobIndx = Object.keys(obj[siteKey])[++lobIndx];
+    app.bottomPerformers = obj[siteKey][newLobIndx].bottomPerformers;
+    runMarquee(obj, siteIndx, ++lobIndx, 7, 0);
+    return;
+  }
+
+  let row = rows[rowIndx];
+
+  if (testIndx == row[7].length) {
+    runMarquee(obj, siteIndx, lobIndx, --rowIndx, 0);
+    return;
+  }
+
+  // Change selected agent
+  app.currentData.site = siteKey;
+  app.currentData.lob = lobKey;
+  app.currentData.agentName = row[2];
+  app.currentData.agentId = row[0];
+  app.currentData.agentPicUrl = row[1];
+  app.currentData.agentRank = row[3];
+  app.scoreNum = row[5];
+  app.currentData.agentTrend = row[4];
+  app.surveysNum = row[6];
+  app.currentData.testimonial = row[7][testIndx] || "";
+  app.currentData.testimonialId = testIndx;
+
+  setTimeout(ev => {
+    runMarquee(obj, siteIndx, lobIndx, rowIndx, ++testIndx);
+  }, 7500);
+}
+
+runMarquee(siteStatsData, 0, 0, 0, 1);
